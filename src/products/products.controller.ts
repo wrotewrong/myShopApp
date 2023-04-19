ba12014db2c421994ea78be5b243182c3069b4db
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-04-19 20:42:23 +0200

    Add products model, controller and service files

---
+++ src/products/products.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Put,
+  Body,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.productsService.getById(id);
+    if (!book) throw new NotFoundException('Book not found');
+    return book;
+  }
+
+  // @Put('/:id')
+  // async update(
+  //   @Param('id', new ParseUUIDPipe()) id: string,
+  //   @Body('orderedAmount') orderedAmount: number,
+  // ) {
+  //   if (!(await this.productsService.getById(id)))
+  //     throw new NotFoundException('Product not found');
+
+  //   await this.productsService.updateAmountById(id, orderedAmount);
+  //   return { success: true };
+  // }
+
+  // @Put('/:id')
+  // async update(
+  //   @Param('id', new ParseUUIDPipe()) id: string,
+  //   @Body('amount') amount: number,
+  // ) {
+  //   if (!(await this.productsService.getById(id)))
+  //     throw new NotFoundException('Product not found');
+
+  //   await this.productsService.updateAmountById(id, amount);
+  //   return { success: true };
+  // }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { Product } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Product[]> {
+    return this.prismaService.product.findMany({ include: { images: true } });
+  }
+
+  public getById(id: Product['id']): Promise<Product | null> {
+    return this.prismaService.product.findUnique({
+      where: { id },
+      include: { images: true },
+    });
+  }
+
+  // public async updateAmountById(
+  //   id: Product['id'],
+  //   orderedAmount: Product['amount'],
+  // ): Promise<Product> {
+  //   try {
+  //     const product = await this.prismaService.product.findUnique({
+  //       where: { id },
+  //     });
+
+  //     const newAmount = product.amount - orderedAmount;
+
+  //     return await this.prismaService.product.update({
+  //       where: { id },
+  //       data: {
+  //         amount: newAmount,
+  //       },
+  //     });
+  //   } catch (error) {
+  //     if (error.code === 'P2025') {
+  //       throw new BadRequestException("Product doesn't exist");
+  //     }
+  //     throw error;
+  //   }
+  // }
+
+  // public async updateAmountById(
+  //   id: Product['id'],
+  //   amount: Product['amount'],
+  // ): Promise<Product> {
+  //   try {
+  //     return await this.prismaService.product.update({
+  //       where: { id },
+  //       data: {
+  //         amount,
+  //       },
+  //     });
+  //   } catch (error) {
+  //     if (error.code === 'P2025') {
+  //       throw new BadRequestException("Product doesn't exist");
+  //     }
+  //     throw error;
+  //   }
+  // }
+}
